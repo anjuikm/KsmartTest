beb8e959c9c9924689013e03086abb8741774c9c
anjuikm <108713715+anjuikm@users.noreply.github.com>
2024-04-05 12:14:30 +0530

    Fine Calculation

--- src/main/java/com/ikm/citizen/service/FineCalculationService.java
+++ src/main/java/com/ikm/citizen/service/FineCalculationService.java
@@ -19,17 +19,16 @@ public class FineCalculationService {
 
     long daysBetween = ChronoUnit.DAYS.between(currentDate, request.getRenewalDateTo());
     double fine = 0.0;
-
     double lateFee = 0.0;
     if (daysBetween < 0) {
       double fineChecker = Double.parseDouble(request.getLicenseFee().toString()) * 3;
       fine = Math.min(fineChecker, 2000.00); // Apply Date is Greater than License Validity To Date 
-
-      double lateFeeFirst = Double.parseDouble(request.getLicenseFee().toString()) * 0.10; //Apply Before Valid To Date and After 29 days 10%
-      lateFee = lateFeeFirst;
-      if(daysBetween<-30){
-        double lateFeeSecond = lateFeeFirst + Double.parseDouble(request.getLicenseFee().toString()) * 0.25; // Calculate First 10 Days 25%
-        long additionalLateDays = (-1 * (daysBetween + 10)) / 15; // Calculate additional late days
+      if (daysBetween > -29) {
+        lateFee = Double.parseDouble(request.getLicenseFee().toString()) * 0.10; //Apply Before Valid To Date and After 29 days 10%
+      }
+      if(daysBetween<-29){
+        double lateFeeSecond = Double.parseDouble(request.getLicenseFee().toString()) * 0.25; // Calculate First 10 Days 25%
+        long additionalLateDays = (-1 * (daysBetween + 40)) / 15; // Calculate additional late days
 
         lateFeeSecond += additionalLateDays * Double.parseDouble(request.getLicenseFee().toString()) * 0.25; // Calculate Each 15 days - 25%
         if ((-1 * (daysBetween + 10)) % 15 > 0) {
